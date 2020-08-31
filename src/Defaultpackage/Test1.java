a9774324b8233087ea94c05a898916b206a52952
saikumar23041995 <saikumarsamineni123@gmail.com>
2020-08-31 19:21:57 +0200

    First commit

---
+++ src/Defaultpackage/Test1.java
@@ -0,0 +1,17 @@
+package Defaultpackage;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		String hungry="yes";
+		
+		if (hungry == "yes")
+			System.out.println("eat something");
+		
+		else
+		{
+			System.out.println("Do work");
+		}
+	}
+
+}
